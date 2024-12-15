dc9c0d2ea3c87701055f8fad4b15e83ca0e46101
Dan <simmons.dan@protonmail.com>
2024-12-15 16:30:22 -0500

    version 1

---
+++ src/raccoon.js
@@ -0,0 +1,10 @@
+const raccoon = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    width: 80,
+    height: 40,
+    speed: 4,
+    attacking: false,
+    image: new Image()
+};
+raccoon.image.src = './assets/ruccoon.png'; // Path to the raccoon image
